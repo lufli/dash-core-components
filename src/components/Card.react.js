bf3c15255b7a89ab0a399579feeafc6fa07910a6
lufli <lufan-li@uiowa.edu>
2018-08-15 10:17:32 -0400

    import twice

--- src/components/Card.react.js
+++ src/components/Card.react.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {Component} from 'react';
+import React, {Component}from 'react';
 import PropTypes from 'prop-types';
 
 export default class Card extends Component {
